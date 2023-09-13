cfbc5d0bb4477b3e5486ac8e652fa38fff06552c
Anderson-431 <anderson.p20@gmail.com>
2023-09-13 20:13:30 -0300

    Ex1 end

--- src/routes/Home/HomeBady/index.tsx
+++ src/routes/Home/HomeBady/index.tsx
@@ -1,20 +1,22 @@
+import { Link } from "react-router-dom";
 import Button from "../../../components/Header/Button";
 import Card from "../../../components/Header/Card";
 
-export default function HomeBody(){
-
-    return (
-        <main>
-            <section>
-                <div className="container">
-                    <div className="mt30 mb30">
-                    <Card title="Página inicial" />
-                    </div>  
-                    <div className="dflex">
-                    <Button text="Ver promoção" />
-                    </div>    
-                </div>                
-            </section>
-        </main>
-    )
+export default function HomeBody() {
+  return (
+    <main>
+      <section>
+        <div className="container">
+          <div className="mt30 mb30">
+            <Card title="Página inicial" />
+          </div>
+          <div className="dflex">
+            <Link to="/promotion">
+              <Button text="Ver promoção" />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
